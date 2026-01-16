3b66d4521150e4277648feb8bcf0609aadf5158d
Nick Whyte <nick@nickwhyte.com>
2026-01-16 21:18:21 +1100

    improved html plotting of plans

--- src/hass_energy/plotting/__init__.py
+++ src/hass_energy/plotting/__init__.py
@@ -1,5 +1,5 @@
 """Plotting utilities."""
 
-from hass_energy.plotting.plan import plot_plan
+from hass_energy.plotting.plan import plot_plan_html
 
-__all__ = ["plot_plan"]
+__all__ = ["plot_plan_html"]
