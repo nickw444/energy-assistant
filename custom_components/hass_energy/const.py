c6687e424c911cc5ddbf41ee19c1a2e6f60a9aca
Fabian Wiles <fabian.wiles@gmail.com>
2026-01-19 10:55:11 +0000

    add docker config

--- custom_components/hass_energy/const.py
+++ custom_components/hass_energy/const.py
@@ -4,6 +4,6 @@ DOMAIN = "hass_energy"
 
 CONF_BASE_URL = "base_url"
 CONF_TIMEOUT = "timeout"
-DEFAULT_BASE_URL = "http://localhost:8000"
+DEFAULT_BASE_URL = "http://localhost:6070"
 DEFAULT_TIMEOUT = 30
 DEFAULT_SCAN_INTERVAL = timedelta(seconds=90)
